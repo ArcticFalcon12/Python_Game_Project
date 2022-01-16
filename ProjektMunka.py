379c26e98b88f7ba96b805eeac839d5b34d11ab5
Marci-420 <meszaros.marcell.zsolt.2020i@bankitatabanya.hu>
2022-01-16 12:47:06 +0100

    Folyosó bal része
    A folyosó bal része készen van

--- ProjektMunka.py
+++ ProjektMunka.py
@@ -44,6 +44,8 @@ def valasztas_kes():
 B. Gyorsan elvágod a kötelet és elbújsz egy közeli szekrényben.""")
  choice = input(">>> ")
  if choice in valasz_A:
+      print("A kést elrejted a párna alá és visszafekszel az ágyra")
+      print("Az ismeretlen idegen bejön a szobába. Mivel az ágyon fekszel, nem gyanakszik semmire. Ezért továbbáll. Ezek után felkelsz az ágyból és kiutat kezdesz keresni. Kilépsz a szobából.")
       folyoso()
  elif choice in valasz_B:
       print("Miután elbújtál meglátod az szekrényajtó rése körül, hogy egy ismeretlen idegen belépett a szobába."
@@ -60,5 +62,55 @@ def valasztas_kez():
  print("Az ismeretlen idegen bejön a szobába. Mivel az ágyon fekszel nem gyanakszik semmire."
 "Ezért továbbáll. Ezekután felkelsz az ágyból, magadhoz veszed a kést és kiutat kezdesz keresni. Kilépsz a szobából.")
 def folyoso():
- print("..")
+ print("Kilépve a szobából egy folyosón találod magad, amely két irányba nyúlik el hosszan.")
+ time.sleep(1)
+ print("""A. Úgy döntesz, hogy balra fordulsz 
+ B. Úgy döntesz, hogy jobbra fordulsz.""")
+ choice = input(">>> ")
+ if choice in valasz_A:
+     valasztas_balra()
+ elif choice in valasz_B:
+    valasztas_jobbra()
+ else: 
+    print(szukseges)
+    folyoso()
+def valasztas_balra():
+ print("Kilépsz a szobából. A folyosó bal része felé haladsz és meglátsz egy szobát, amiből fény szűrődik ki.")
+ time.sleep(1)
+ print(""" A. Bemész a szobába
+B. Tovább haladsz a folyosón""")
+ choice = input(">>> ")
+ if choice in valasz_A:
+   valasztas_szoba()
+ elif choice in valasz_B:
+    valasztas_tovabb()
+ else:
+    print(szukseges)
+    valasztas_balra()
+def valasztas_szoba():
+    print("A szobába belépve megtalálod, hogy a fény forrása a telefonod. Közeleb lépve látod, hogy egy videó megy. A videón a te az újévi fogadalmadról beszélsz, miközben a háttérben neked kiabálnak és füttyögnek. A videó azzal zárul, hogy stoppolsz és egy fekete furgont látsz meg. Elszörnyülködsz azon, hogy mennyire ostoba voltál, majd elrakod a telefonod. Ezután visszaindulsz a folyosóra.  ")
+    time.sleep(5)
+    valasztas_tovabb()
+def valasztas_tovabb():
+ print("A folyosó végén egy szobát találsz tárva nyitva. A szoba nincs megvilágítva, de a félhomályban észreveszel egy lógó valamit, ami mintha lebegne. Rossz előérzeted van, ugyanakkor kíváncsi vagy mi van bent.")
+ time.sleep(1)
+ print("""A. Bemész a szobába
+B. Nem mész be a szobába""")
+ choice = input(">>> ")
+ if choice in valasz_A:
+  valasztas_bemesz_kinzo()
+ elif choice in valasz_B:
+  valasztas_halal()
+ else:
+    print(szukseges)
+    valasztas_tovabb()
+def valasztas_bemesz_kinzo():
+    print("Kiváncsi vagy, hogy mi van a szobában, azonban az elviselhetetlen bűzön és a tócsákon kívűl nem érzel mást. Fényre van szükséged, ezért felkapcsolód a lámpát.")
+    print("Hirtelen sokkot kapsz. Rájössz, hogy a szag, amit éreztél (valami durván kiiratni) hullaszag volt, a test, amit láttál lógni, egy felkoncolt fiatal lány volt és a tócsa, amit éreztél a lányból folyó vér volt.  A szoba közepén egy megviselt fogorvosi szék fogad, mellette pedig egy asztal, amin különböző kínzó eszközöket találtál. A fogorvosi széktől balra még egy asztalt találtál, amin csontdarabok és bőrcafatokat találhatóak. Könyvek csillantak fel a szemed sarkában, aminek a címei Légy az eleségem, vagy Egyem a húsodat, Pizzafutár és egyéb gyorsételek. Miután körülnéztél a szobába a férfi benyit, és leüt egy balta markolatával. ")
+    valasztas_finale()
+def valasztas_halal():
+    print("Úgy döntesz, hogy nem mész be. Jobbra tekintve egy másik helyiséget találsz. Hangokat hallasz magad mögött, hátra tekintve árnyakat látsz ezért félelmedben benyitsz a szobába. Az ajtóban egy nyitott medve csapda várta áldozatát és te sietségedben nem vetted észre, így az levágta a lábadat és elvéreztél.")
+time.sleep(3)
+print("Meghaltál.")
+
 intro()
